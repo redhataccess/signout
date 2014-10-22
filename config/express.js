5a14a1611490c571ec5fba49f9b972b5d171e5dc
Connor Hindley <conn.hindley@gmail.com>
2014-10-22 09:33:52 -0400

    add image path

--- config/express.js
+++ config/express.js
@@ -2,17 +2,31 @@
 var express = require('express');
 
 
+function clearCookies(req, res) {
+  for (var cookie in req.cookies) {
+    res.cookie(cookie, '', {
+      expires: new Date(1),
+      path: '/',
+      domain: '.redhat.com'
+    });
+  }
+}
+
 module.exports = function(app, config) {
+
+  app.get('/img.gif', function(req, res) {
+    var img = new Buffer('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64');
+    res.writeHead(200, {
+      'Content-Type': 'image/gif',
+      'Content-Length': img.length
+    });
+    clearCookies(req, res);
+    res.end(img);
+  });
 
   app.all('*', function(req, res) {
     res.header('Access-Control-Allow-Origin', '*');
-    for (var cookie in req.cookies) {
-      res.cookie(cookie, '', {
-        expires: new Date(1),
-        path: '/',
-        domain: '.redhat.com'
-      });
-    }
+    clearCookies(req, res);
     if (req.method === 'GET') {
       res.redirect('https://access.redhat.com');
     } else {
